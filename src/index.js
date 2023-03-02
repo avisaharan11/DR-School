6a09d459d0e2052fb96d94e496bb2859b759e70f
Abhimanu Saharan <avisaharan1@gmail.com>
2023-03-02 07:38:08 +0530

    hash routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </BrowserRouter>
+  </HashRouter>
   //</React.StrictMode>
 );
